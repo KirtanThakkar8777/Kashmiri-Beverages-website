564c21db552ef17c2896b1da25bb6e9cbb568d8a
KirtanThakkar8777 <kirtanthakkar2835@gmail.com>
2026-02-15 22:52:03 +0530

    fix static path

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser";
 import adminAuth from "./middleware/auth.middleware.js"
 import dns from "node:dns";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middlewares
 app.use(cors({
@@ -113,7 +116,7 @@ if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
